f92c64b1c9985c4224694ddf7f76e9d2ce0a244c
Barnomoy <barnomoydatta10@gmail.com>
2026-01-20 10:27:40 +0000

    Add diagonals and wwrite code for DOM
    - Add diagonal classes to the corresponding boxes in the index file
    - Write code for the working game in the DOM

--- script.js
+++ script.js
@@ -1,267 +1,284 @@
-const game = ( () => {
-
-  // Create an empty array for the console gameboard
-  const gameboardArr = ["", "", "", "", "", "", "", "", ""];
-  
-  // Create a player1 object with their name and marker
+const game = (function () {
   const createPlayer1 = (name) => {
     const marker = "X";
-    const array = ["", "", "", "", "", "", "", "", ""];
-    return { name, marker, array };
-  }
+    return { name, marker };
+  };
 
-  // Create a player2 oject with their name and marker
   const createPlayer2 = (name) => {
     const marker = "O";
-    const array = ["", "", "", "", "", "", "", "", ""];
-    return { name, marker, array };
-  }
+    return { name, marker };
+  };
 
-  // Create a gameflow object to control the flow of the game
   const gameflow = () => {
+    let player1Name = prompt("Enter the name of Player 1:");
+    let player2Name = prompt("Enter the name of Player 2:");
 
-    // Prompt the players for their name
-    let player1Name = window.prompt("Enter the name of Player 1:");
-    let player2Name = window.prompt("Enter the name of Player 2:");
+    const Player1 = createPlayer1(player1Name);
+    const Player2 = createPlayer2(player2Name);
 
-    // Call the players using their names
-    const player1 = createPlayer1(player1Name);
-    const player2 = createPlayer2(player2Name);
+    const boxes = document.querySelectorAll(".box");
 
-    // Create a array variable to run a loop till the end of the array
-    const gameboardLength = gameboardArr.length;
-    let arrayVar;
-    if (gameboardLength % 2 === 1) {
-      arrayVar = (gameboardLength + 1)/2;
+    let currentPlayer = Player1;
+    let totalMoves = 0;
+
+    function checkPlayerTurn(event) {
+      if (currentPlayer === Player1) {
+        if (event.target.innerText === "") {
+          event.target.innerText = Player1.marker;
+          totalMoves++;
+          currentPlayer = Player2;
+        }
+      } else if (currentPlayer === Player2) {
+        if (event.target.innerText === "") {
+          event.target.innerText = Player2.marker;
+          totalMoves++;
+          currentPlayer = Player1;
+        }
+      }
+
+      if (totalMoves >= 5) {
+        function checkRows() {
+          function checkFirstRow() {
+            let player1Moves = 0;
+            let player2Moves = 0;
+
+            const firstRows = document.querySelectorAll(".first-row");
+
+            firstRows.forEach((firstRow) => {
+              if (firstRow.innerText === Player1.marker) {
+                player1Moves++;
+              } else if (firstRow.innerText === Player2.marker) {
+                player2Moves++;
+              }
+            });
+
+            if (player1Moves === 3) {
+              return Player1;
+            } else if (player2Moves === 3) {
+              return Player2;
+            }
+          }
+
+          function checkSecondRow() {
+            let player1Moves = 0;
+            let player2Moves = 0;
+
+            const secondRows = document.querySelectorAll(".second-row");
+
+            secondRows.forEach((secondRow) => {
+              if (secondRow.innerText === Player1.marker) {
+                player1Moves++;
+              } else if (secondRow.innerText === Player2.marker) {
+                player2Moves++;
+              }
+            });
+
+            if (player1Moves === 3) {
+              return Player1;
+            } else if (player2Moves === 3) {
+              return Player2;
+            }
+          }
+
+          function checkThirdRow() {
+            let player1Moves = 0;
+            let player2Moves = 0;
+
+            const thirdRows = document.querySelectorAll(".third-row");
+
+            thirdRows.forEach((thirdRow) => {
+              if (thirdRow.innerText === Player1.marker) {
+                player1Moves++;
+              } else if (thirdRow.innerText === Player2.marker) {
+                player2Moves++;
+              }
+            });
+
+            if (player1Moves === 3) {
+              return Player1;
+            } else if (player2Moves === 3) {
+              return Player2;
+            }
+          }
+
+          const firstRow = checkFirstRow();
+          const secondRow = checkSecondRow();
+          const thirdRow = checkThirdRow();
+
+          if (
+            firstRow === Player1 ||
+            secondRow === Player1 ||
+            thirdRow === Player1
+          ) {
+            return Player1;
+          } else if (
+            firstRow === Player2 ||
+            secondRow === Player2 ||
+            thirdRow === Player2
+          ) {
+            return Player2;
+          }
+        }
+        function checkColumns() {
+          function checkFirstColumn() {
+            let player1Moves = 0;
+            let player2Moves = 0;
+
+            const firstColumns = document.querySelectorAll(".first-column");
+
+            firstColumns.forEach((firstColumn) => {
+              if (firstColumn.innerText === Player1.marker) {
+                player1Moves++;
+              } else if (firstColumn.innerText === Player2.marker) {
+                player2Moves++;
+              }
+            });
+            if (player1Moves === 3) {
+              return Player1;
+            } else if (player2Moves === 3) {
+              return Player2;
+            }
+          }
+          function checkSecondColumn() {
+            let player1Moves = 0;
+            let player2Moves = 0;
+
+            const secondColumns = document.querySelectorAll(".second-column");
+
+            secondColumns.forEach((secondColumn) => {
+              if (secondColumn.innerText === Player1.marker) {
+                player1Moves++;
+              } else if (secondColumn.innerText === Player2.marker) {
+                player2Moves++;
+              }
+            });
+            if (player1Moves === 3) {
+              return Player1;
+            } else if (player2Moves === 3) {
+              return Player2;
+            }
+          }
+          function checkThirdColumn() {
+            let player1Moves = 0;
+            let player2Moves = 0;
+
+            const thirdColumns = document.querySelectorAll(".third-column");
+
+            thirdColumns.forEach((thirdColumn) => {
+              if (thirdColumn.innerText === Player1.marker) {
+                player1Moves++;
+              } else if (thirdColumn.innerText === Player2.marker) {
+                player2Moves++;
+              }
+            });
+            if (player1Moves === 3) {
+              return Player1;
+            } else if (player2Moves === 3) {
+              return Player2;
+            }
+          }
+
+          const firstColumn = checkFirstColumn();
+          const secondColumn = checkSecondColumn();
+          const thirdColumn = checkThirdColumn();
+
+          if (
+            firstColumn === Player1 ||
+            secondColumn === Player1 ||
+            thirdColumn === Player1
+          ) {
+            return Player1;
+          } else if (
+            firstColumn === Player2 ||
+            secondColumn === Player2 ||
+            thirdColumn === Player2
+          ) {
+            return Player2;
+          }
+        }
+        function checkDiagonals() {
+          function checkFirstDiagonal() {
+            let player1Moves = 0;
+            let player2Moves = 0;
+
+            const firstDiagonals = document.querySelectorAll(".first-diagonal");
+
+            firstDiagonals.forEach((firstDiagonal) => {
+              if (firstDiagonal.innerText === Player1.marker) {
+                player1Moves++;
+              } else if (firstDiagonal.innerText === Player2.marker) {
+                player2Moves++;
+              }
+            });
+
+            if (player1Moves === 3) {
+              return Player1;
+            } else if (player2Moves === 3) {
+              return Player2;
+            }
+          }
+          function checkSecondDiagonal() {
+            let player1Moves = 0;
+            let player2Moves = 0;
+
+            const secondDiagonals =
+              document.querySelectorAll(".second-diagonal");
+
+            secondDiagonals.forEach((secondDiagonal) => {
+              if (secondDiagonal.innerText === Player1.marker) {
+                player1Moves++;
+              } else if (secondDiagonal.innerText === Player2.marker) {
+                player2Moves++;
+              }
+            });
+
+            if (player1Moves === 3) {
+              return Player1;
+            } else if (player2Moves === 3) {
+              return Player2;
+            }
+          }
+
+          const firstDiagonal = checkFirstDiagonal();
+          const secondDiagonal = checkSecondDiagonal();
+
+          if (firstDiagonal === Player1 || secondDiagonal === Player1) {
+            return Player1;
+          } else if (firstDiagonal === Player2 || secondDiagonal === Player2) {
+            return Player2;
+          }
+        }
+
+        const rows = checkRows();
+        const columns = checkColumns();
+        const diagonals = checkDiagonals();
+
+        if (rows === Player1 || columns === Player1 || diagonals === Player1) {
+          alert(`${Player1.name} wins!`);
+
+          boxes.forEach((box) => {
+            box.removeEventListener("click", checkPlayerTurn);
+          });
+        } else if (
+          rows === Player2 ||
+          columns === Player2 ||
+          diagonals === Player2
+        ) {
+          alert(`${Player2.name} wins!`);
+
+          boxes.forEach((box) => {
+            box.removeEventListener("click", checkPlayerTurn);
+          });
+        } else if (totalMoves === boxes.length) {
+          alert("No one wins!");
+        }
+      }
     }
-    else {
-      arrayVar = gameboardLength/2;
-    }
 
-    // Create a move counter variable to track the number of moves by the players
-    let playerMoveCounter = 0;
-
-    // Run a for loop till the end of the half of the array
-    for (let i = 0; i < arrayVar; i++) {
-      // Prompt the player 1 for their input
-      const player1Input = prompt("Enter the position for the marker of Player 1:");
-
-      // Validate that the player input is a proper number
-      if (player1Input > 0 && player1Input <= gameboardLength) {
-        const arrPosition = player1Input - 1;
-        if (gameboardArr[arrPosition] == "") {
-          gameboardArr.splice(arrPosition, 1, player1.marker);
-          player1.array.splice(arrPosition, 1, player1.marker);
-          console.log(gameboardArr);
-        } else {
-          alert("The position in the array is filled");
-        }
-      } else {
-        console.error("The number is beyond the scope of the array");
-      }
-
-      // Increment the counter variable after player 1 inserts their input
-      playerMoveCounter++;
-
-      // Check if the total game moves are above the minimum threshold
-      if (playerMoveCounter >= arrayVar) {
-        // Create a function to check the horizontal matches of the player
-        const player1Horizontal = () => {
-          for (let i = 0; i < gameboardLength; i+=3) {
-            let x = 0;
-            let k = i + 3;
-            for (let j = i; j < k; j++) {
-              if (player1.array[j] === player1.marker) {
-                x++;            
-              }
-              if (x === 3) {
-                return x;
-              }
-            }
-          }
-          return;
-        }
-
-        // Create a function to check the vertical matches of the player
-        const player1Vertical = () => {
-          for (let i = 0; i < 3; i++) {
-            let x = 0;
-            let k = i + 7;
-            for (let j = i; j < k; j+=3) {
-              if (player1.array[j] === player1.marker) {
-                x++;            
-              }
-              if (x === 3) {
-                return x;
-              }
-            }
-          }
-          return;
-        }
-
-        // Create a function to check the diagonal matches of the player
-        const player1Diagonal = () => {
-          let x = 0; y = gameboardLength;
-          if (player1.array[x] === player1.marker) {
-            let c = 0;
-            for (let i = x; i < y; i+= 4) {
-              if (player1.array[i] === player1.marker) {
-                c++;
-              }
-            }
-            if (c === 3) {
-              return c;
-            }
-          }
-          x = x + 2;
-          y = y - 2;
-          if (player1.array[x] === player1.marker) {
-            let c = 0;
-            for (let i = x; i < y; i+=2) {
-              if (player1.array[i] === player1.marker) {
-                c++;
-              }
-            }
-            if (c === 3) {
-              return c;
-            }
-          }
-          return;
-        }
-        
-        // Check if the player matches the horizontal function
-        if (player1Horizontal() === 3) {
-          console.log(`${player1.name} Wins!`);
-          break;
-        }
-
-        // Check if the player matches the vertical function
-        else if (player1Vertical() === 3) {
-          console.log(`${player1.name} Wins!`);
-          break;
-        }
-
-        // Check if the player matches the diagonal function
-        else if (player1Diagonal() === 3) {
-          console.log(`${player1.name} Wins!`);
-          break;
-        }
-
-        // Check if the total moves is equal to the length of the array
-        else if (playerMoveCounter === gameboardLength) {
-          console.log("No one Wins!");
-          break;
-        }
-      }
-
-      // Prompt the player 2 for their input
-      const player2Input = prompt("Enter the position for the marker of Player 2:");
-
-      // Validate that the player input is a proper number
-      if (player2Input > 0 && player2Input <= gameboardLength) {
-        const arrPosition = player2Input - 1;
-        if (gameboardArr[arrPosition] == "") {
-          gameboardArr.splice(arrPosition, 1, player2.marker);
-          player2.array.splice(arrPosition, 1, player2.marker);
-          console.log(gameboardArr);
-        } else {
-          alert("The position in the array is filled");
-        }
-      } else {
-        console.error("The number is beyond the scope of the array");
-      }
-
-      // Increment the counter variable after player 2 inserts their input 
-      playerMoveCounter++;
-
-      // Check if the total game moves are above the minimum threshold
-      if (playerMoveCounter >= arrayVar) {
-
-        // Create a function to check the horizontal matches of the player
-        const player2Horizontal = () => {
-          for (let i = 0; i < gameboardLength; i+=3) {
-            let x = 0;
-            let k = i + 3;
-            for (let j = i; j < k; j++) {
-              if (player2.array[j] === player2.marker) {
-                x++;            
-              }
-              if (x === 3) {
-                return x;
-              }
-            }
-          }
-          return;
-        }
-
-        // Create a function to check the vertical matches of the player
-        const player2Vertical = () => {
-          for (let i = 0; i < 3; i++) {
-            let x = 0;
-            let k = i + 7;
-            for (let j = i; j < k; j+=3) {
-              if (player2.array[j] === player2.marker) {
-                x++;            
-              }
-              if (x === 3) {
-                return x;
-              }
-            }
-          }
-          return;
-        }
-
-        // Create a function to check the diagonal matches of the player
-        const player2Diagonal = () => {
-          let x = 0; y = gameboardLength;
-          if (player2.array[x] === player2.marker) {
-            let c = 0;
-            for (let i = x; i < y; i+= 4) {
-              if (player2.array[i] === player2.marker) {
-                c++;
-              }
-            }
-            if (c === 3) {
-              return c;
-            }
-          }
-          x = x + 2;
-          y = y - 2;
-          if (player2.array[x] === player2.marker) {
-            let c = 0;
-            for (let i = x; i < y; i+=2) {
-              if (player2.array[i] === player2.marker) {
-                c++;
-              }
-            }
-            if (c === 3) {
-              return c;
-            }
-          }
-          return;
-        }
-        
-        // Check if the player matches the horizontal function
-        if (player2Horizontal() === 3) {
-          console.log(`${player2.name} Wins!`);
-          break;
-        }
-
-        // Check if the player matches the vertical function
-        else if (player2Vertical() === 3) {
-          console.log(`${player2.name} Wins!`);
-          break;
-        }
-
-        // Check if the player matches the horizontal function
-        else if (player2Diagonal() === 3) {
-          console.log(`${player2.name} Wins!`);
-          break;
-        }
-      }
-    }
-  }
-  return { gameflow }
+    boxes.forEach((box) => {
+      box.addEventListener("click", checkPlayerTurn);
+    });
+  };
+  return { gameflow };
 })();
-
 game.gameflow();
